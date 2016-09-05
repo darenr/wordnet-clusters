57d099faf541236ce332a11f613d4b3fae903b29
drace <daren.race@oracle.com>
2016-09-05 16:39:17 -0700

    added print utility

---
+++ print_clusters.py
@@ -0,0 +1,18 @@
+import json
+import sys
+
+if __name__ == "__main__":
+  if len(sys.argv) == 2:
+    with open(sys.argv[1]) as f:
+      print sys.argv[1]
+      j = json.loads(f.read())
+      for cluster in j:
+        print 
+        print cluster
+        if len(j[cluster]) < 20 and len(j[cluster]) > 3:
+          for word in sorted(j[cluster]):
+            print "  ", word
+        
+  else:
+    print "usage: <json file>"
+
